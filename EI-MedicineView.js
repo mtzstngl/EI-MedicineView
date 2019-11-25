659b08d9b5bc6f5f5093ffd9649cb819ef0df672
Matze Stangl <matze749@gmail.com>
2019-11-25 20:59:12 +0100

    Add medicine infos

--- EI-MedicineView.js
+++ EI-MedicineView.js
@@ -1,6 +1,9 @@
 "use strict";
 
 Module.register("EI-MedicineView", {
+
+	medicine: null,
+
 	start: function() {
 		Log.info("Starting module: " + this.name);
 	},
@@ -9,11 +12,26 @@ Module.register("EI-MedicineView", {
 		return "ei_medicineView.njk";
 	},
 
-	/*getTemplateData: function () {
-		let data = {
-			oth_image: this.file("othaw-logo.svg"),
-			epp_image: this.file("epp-logo.jpg")
-		};
-		return data;
-	}*/
+	getTemplateData: function() {
+		return self.medicine;
+	},
+
+	resume: function() {
+		this.sendNotification("MED_START_SCANNING");
+	},
+
+	suspend: function() {
+		this.sendNotification("MED_STOP_SCANNING");
+	},
+
+	notificationReceived: function(notification, payload, sender) {
+		const self = this;
+
+		switch (notification) {
+		case "MEDICINE":
+			self.medicine = payload;
+			self.updateDom();
+			break;
+		}
+	},
 });
